3e290435c5cefcefe889f39678b914070ebc9a68
kayac-chang <egg734631@gmail.com>
2022-04-26 22:30:54 +0800

    feat: 🎸 add readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Word Prediction
+
+word prediction using [Markov Chain][markov_chain]
+
+[markov_chain]: https://en.wikipedia.org/wiki/Markov_chain
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,8 @@
 export type ValueOf<T> = T extends Record<any, infer V> ? V : never;
 
-export type Lexicon = Record<string, { [key: string]: number }[]>;
+export type Lexicon = Record<string, Record<string, number>>;
+
+export type Suggest = {
+  word: string;
+  weight: number;
+};
